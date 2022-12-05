0704d7d9dcfe2ec3c5888b942ede6729c5d66974
Morten Bendiksen <morten.bendiksen@gmail.com>
2022-12-05 15:47:48 +0100

    day 5

--- aoc.rkt
+++ aoc.rkt
@@ -11,9 +11,23 @@
               (loop (cons next-line lines) ; else loop, keeping this line
                     (read-line))))))))       ; and move to next one the file's contents
 
+(define (my-drop list n)
+  (if (or (empty? list) (= n 0)) list (my-drop (rest list) (- n 1))))
+
+(define (my-take list n)
+  (let loop ([list list][n n][acc '()])
+    (if (or (empty? list) (= n 0)) (reverse acc) (loop (rest list) (- n 1) (cons (first list) acc)))))
+
+(define (sliding list n)
+  (let loop ([list list][acc empty])
+    (if (null? list) 
+      (reverse acc)
+      (loop (my-drop list n) (cons (my-take list n) acc)))))
+
 
 (define (insp val)
   (display val)
+  (display "\n")
   val)
 
 
---
+++ day5/part1.rkt
@@ -0,0 +1,62 @@
+#lang racket
+(require "../aoc.rkt")
+
+(define (countstacks lines heigth)
+  (if
+   (number? (string->number (first (string-split (first lines)))))
+   (values
+    (length (string-split (first lines)))
+    heigth)
+   (countstacks (rest lines) (+ 1 heigth))))
+      
+
+(define (fillstacks lines stacks n)
+  (if (= n 0)
+   stacks
+   (let* ([as-list (string->list (first lines))]
+          [elems (map first (sliding (rest as-list) 4))])
+     (fillstacks (rest lines) (cons elems stacks) (- n 1)))))
+  
+
+(define (readstacks lines)
+  (let*-values ([(n h) (countstacks lines 0)]
+                [(stacks) (fillstacks lines '() n)])
+    stacks))
+
+(define (readmoves lines)
+  (let loop ([lines lines][acc '()])
+    (if (null? lines)
+        (reverse acc)
+        (match (regexp-match #rx"move ([0-9]+) from ([0-9]+) to ([0-9]+)" (first lines))
+          [#f (loop (rest lines) acc)]
+          [(list _ a b c) (loop (rest lines) (cons (map string->number (list a b c)) acc))]))))
+
+(define (transpose xss)
+  (apply map list xss))
+
+(define (move-one stacks from to)
+  (insp stacks)
+  (let* ([from (- from 1)]
+         [to (- to 1)]
+         [from-stack (list-ref stacks from)]
+         [to-stack (list-ref stacks to)]
+         [item (first from-stack)]
+         [removed (list-set stacks from (rest from-stack))]
+         [added-back (list-set removed to (cons item to-stack))])
+  added-back))
+
+(define (move inst stacks)
+  (if (= (first inst) 0)
+    stacks
+    (move (cons (- (first inst) 1) (rest inst)) (move-one stacks (second inst) (third inst)) )))
+
+(define (remove-spaces list)
+  (filter (negate (curry char=? #\space)) list))
+
+(define (solve lines)
+  (let* ([stacks (map reverse (map remove-spaces (transpose (readstacks lines))))]
+         [moves (insp (readmoves lines))]
+         [after-moves (foldl move stacks moves)])
+    (list->string (map first after-moves))))
+
+(call-with-lines "input" solve)
